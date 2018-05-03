6c532e8d72badd26b82dc811707981dbbed8fd6c
Vladislav Dmitriyev <d7vladislav@gmail.com>
2018-05-03 10:27:08 +0300

    Bumped version to 1.2.1

--- version/version.go
+++ version/version.go
@@ -2,11 +2,11 @@ package version
 
 const Major = "1"
 const Minor = "2"
-const Fix = "0"
+const Fix = "1"
 
 var (
 	// Version is the current version of Leadschain platform
-	Version = "1.2.0"
+	Version = "1.2.1"
 
 	// GitCommit is the current HEAD set using ldflags.
 	GitCommit string
