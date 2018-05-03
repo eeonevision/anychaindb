696fed50d588eb02f7a8e3039ec79315b1172d33
Vladislav Dmitriyev <d7vladislav@gmail.com>
2018-05-03 11:48:56 +0300

    Added missing 'prune' to docker image command

--- deploy/DOCKER/deploy.sh
+++ deploy/DOCKER/deploy.sh
@@ -80,7 +80,7 @@ clean () {
     docker stop $(docker ps | grep "leadschain-" | awk '/ / { print $1 }')
     docker rm $(docker ps -a | grep "leadschain-" | awk '/ / { print $1 }')
     docker volume rm $(docker volume ls -qf dangling=true)
-    docker image -a -f
+    docker image prune -a -f
     docker system prune -f
 }
 
